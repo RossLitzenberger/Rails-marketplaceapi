8aad5c8f34a95d4d5bc8ce56ef8cb89c58bb36df
Ross Litz <codingclasses.049@gmail.com>
2015-07-05 10:14:08 -0500

    Adds destroy session action added

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -11,7 +11,13 @@ class Api::V1::SessionsController < ApplicationController
       user.save
       render json: user, status: 200, location: [:api, user]
     else
-      render json: { errors: "Invalid email or password"}, status: 422
+      render json: { errors: "Invalid email or password" }, status: 422
     end
+  end
+  def destroy
+    user = User.find_by(auth_token: params[:id])
+    user.generate_authentication_token!
+    user.save
+    head 204
   end
 end
