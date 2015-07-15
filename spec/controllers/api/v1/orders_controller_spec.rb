8d7bb99362f2cd1d5d4270ca0f23fa741ccab573
Ross Litzenberger <codingclasses.049@gmail.com>
2015-07-15 14:16:26 -0500

    Allows the order to be placed along with product quantity

--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -18,13 +18,13 @@ describe Api::V1::OrdersController do
   end
   describe "GET #show" do
     before(:each) do
-    current_user = FactoryGirl.create :user
-    api_authorization_header current_user.auth_token
+      current_user = FactoryGirl.create :user
+      api_authorization_header current_user.auth_token
 
-    @product = FactoryGirl.create :product
-    @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
-    get :show, user_id: current_user.id, id: @order.id
-  end
+      @product = FactoryGirl.create :product
+      @order = FactoryGirl.create :order, user: current_user, product_ids: [@product.id]
+      get :show, user_id: current_user.id, id: @order.id
+    end
 
     it "returns the user order record matching the id" do
       order_response = json_response[:order]
@@ -50,7 +50,7 @@ describe Api::V1::OrdersController do
 
       product_1 = FactoryGirl.create :product
       product_2 = FactoryGirl.create :product
-      order_params = { product_ids: [product_1.id, product_2.id] }
+      order_params = { product_ids_and_quantities: [[product_1.id, 2], [product_2.id, 3]] }
       post :create, user_id: current_user.id, order: order_params
     end
 
@@ -59,6 +59,11 @@ describe Api::V1::OrdersController do
       expect(order_response[:id]).to be_present
     end
 
+    it "embeds the two product objects related to the order" do
+      order_response = json_response[:order]
+      expect(order_response[:products].size).to eql 2
+    end
+    
     it { should respond_with 201 }
   end
 end
